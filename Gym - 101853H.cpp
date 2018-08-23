8d70a3d4119fffe94851e90f7a8ebcc146d098ee
Pulak Kanti Bhowmick <bhowmick8086@gmail.com>
2018-08-23 16:21:13 +0600

    Add files via upload

---
+++ Gym - 101853H.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define sc1(n) scanf("%lld",&n)
+#define sc2(a,b) scanf("%lld%lld",&a,&b)
+#define sc3(a,b,c) scanf("%lld%lld%lld",&a,&b,&c)
+#define MX 100000
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ul;
+
+int main()
+{
+    ll test;
+    sc1(test);
+    while(test--)
+    {
+        ll n;
+        sc1(n);
+        n=n/6;
+        ll ans=sqrt(n);
+        printf("%lld\n",ans);
+    }
+    return 0;
+}
