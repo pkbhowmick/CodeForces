511d06f46f1e4bc89ebc2e8f4d66baa559dbd07a
Pulak Kanti Bhowmick <bhowmick8086@gmail.com>
2018-08-26 16:03:22 +0600

    Add files via upload

---
+++ 506A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define sc1(n) scanf("%lld",&n)
+#define sc2(a,b) scanf("%lld%lld",&a,&b)
+#define sc3(a,b,c) scanf("%lld%lld%lld",&a,&b,&c)
+#define MX 100000
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ul;
+
+int main()
+{
+
+    return 0;
+}
+
